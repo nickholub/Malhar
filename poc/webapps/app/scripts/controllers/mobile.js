ed08b0a14601d00a190dce0d9b151b69d254ed27
nickholub <nikholub@gmail.com>
2013-10-18 11:14:29 -0700

    fix #154 Web Apps - Mobile Locator Demo

--- poc/webapps/app/scripts/controllers/mobile.js
+++ poc/webapps/app/scripts/controllers/mobile.js
@@ -9,8 +9,8 @@ function translateLatLong(item) {
     var phone = parseInt(item.phone);
 
     //TODO magic numbers
-    var latitude = 37.375894 + (phone % 4 - 2) * 0.01 - lat * 0.005;
-    var longitude = -121.959328 + (phone % 8 - 4) * 0.01 + lon * 0.007;
+    var latitude = 37.36197126180853 + (phone % 4 - 2) * 0.01 - lat * 0.005;
+    var longitude = -121.92674696445465 + (phone % 8 - 4) * 0.01 + lon * 0.007;
 
     return { latitude: latitude, longitude: longitude, label: item.phone };
 }
