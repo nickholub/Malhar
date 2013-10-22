1e6ccb0002f0cea10010ed45c9da30f5351864b2
nickholub <nikholub@gmail.com>
2013-10-22 10:57:46 -0700

    fix #178 Web Apps - License Headers

--- poc/webapps/app/scripts/controllers/mobile.js
+++ poc/webapps/app/scripts/controllers/mobile.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*global angular, jQuery, _*/
 (function () {
 'use strict';
@@ -31,6 +47,7 @@ angular.module('mobile')
         var topic = "demos.mobile.phoneLocationQueryResult";
         socket.subscribe(topic, function(message) {
             var item = message.data;
+            console.log(item);
 
             // allow adding phones only if they were not recently removed
             if (removed.hasOwnProperty(item.phone)) {
@@ -47,10 +64,10 @@ angular.module('mobile')
             }
         });
 
-        $scope.$on('phone_added', function (event, phone) {
-            map[phone] = { phone: phone };
-            $scope.$broadcast('datachanged', map);
-        });
+        //$scope.$on('phone_added', function (event, phone) {
+        //    map[phone] = { phone: phone };
+        //    $scope.$broadcast('datachanged', map);
+        //});
 
         $scope.$on('phone_removed', function (event, phone) {
             removed[phone] = Date.now();
