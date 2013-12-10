9b8aead18d88041c5f4130dd23ee646b0eac084a
Nick Holub <nikholub@gmail.com>
2013-12-09 18:21:03 -0800

    fix #436 Map Reduce Monitor - Job Progress Grid

--- web/mrmonitor/app/scripts/controllers/main.js
+++ web/mrmonitor/app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app.controller', ['app.service']);
+angular.module('app.controller', ['ngGrid', 'app.service']);
 
 angular.module('app.controller')
   .controller('MainCtrl', function ($scope, webSocket) {
@@ -23,4 +23,83 @@ angular.module('app.controller')
       $scope.gaugeValue = item.value;
       $scope.$apply();
     });
+  })
+  .controller('JobGridController', function($scope, $filter, webSocket) {
+    console.log('JobGridController');
+    var defaultRow = {
+      completed: '-',
+      running: '-',
+      total: '-',
+      progress: 0
+    };
+
+    function createRow(data) {
+      var row = jQuery.extend({}, data);
+      if (typeof data.progress !== 'undefined') {
+        row.progress = $filter('number')(data.progress, 2) + '%'; //TODO create custom filter
+      } else {
+        row.progress = '100.00%';
+      }
+
+      return row;
+    }
+
+    $scope.gridData = [
+      createRow(jQuery.extend({ name: 'map' }, defaultRow)),
+      createRow(jQuery.extend({ name: 'reduce' }, defaultRow)),
+      createRow(jQuery.extend({ name: 'total' }, defaultRow))
+    ];
+
+    var topic = "contrib.summit.mrDebugger.jobResult";
+    var msg = JSON.stringify({ "type":"subscribe", "topic": topic});
+
+    //socket.send(msg);
+
+    if (false) //TODO
+    webSocket.on(topic, function(message) {
+      var data = JSON.parse(message.data);
+      var job = data.job;
+
+      var list = [];
+      var map = {
+        name: 'map',
+        completed: job.mapsCompleted,
+        running: job.mapsRunning,
+        total: job.mapsTotal,
+        progress: job.mapProgress
+      };
+      var reduce = {
+        name: 'reduce',
+        completed: job.reducesCompleted,
+        running: job.reducesRunning,
+        total: job.reducesTotal,
+        progress: job.reduceProgress
+      };
+      var total = {
+        name: 'total',
+        completed: job.mapsCompleted + job.reducesCompleted,
+        running: job.mapsRunning + job.reducesRunning,
+        total: job.mapsTotal + job.reducesTotal
+      };
+      total.progress = (total.total === 0) ? 0 : (total.completed / total.total * 100);
+
+      list.push(createRow(map));
+      list.push(createRow(reduce));
+      list.push(createRow(total));
+
+      $scope.gridData = list;
+      $scope.$apply();
+    });
+
+    $scope.gridOptions = {
+      data: 'gridData',
+      columnDefs: [
+        { field: "name", displayName: 'Task'},
+        { field: "completed", displayName: 'Completed' },
+        { field: "running", displayName: 'Running' },
+        { field: "total", displayName: 'Total' },
+        { field: "progress", displayName: 'Progress', cellFilter1: 'number:2' }
+      ]
+    };
+
   });
