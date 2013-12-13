1d7601a757d7d6a991202f0394530f0dff3095bc
Nick Holub <nikholub@gmail.com>
2013-12-12 17:13:28 -0800

    fix #474 Map Reduce Monitor - AngularJS UI-Router

--- web/mrmonitor/app/scripts/app.js
+++ web/mrmonitor/app/scripts/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
-angular.module('app', ['ngRoute', 'app.service', 'app.directive', 'app.controller', 'app.filter', 'ui.bootstrap']);
+angular.module('app', ['ui.router', 'app.service', 'app.directive', 'app.controller', 'app.filter', 'ui.bootstrap']);
 
 angular.module('app')
-  .config(function ($routeProvider, webSocketProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, webSocketProvider) {
     //TODO
     //webSocketProvider.setWebSocketURL('ws://' + window.location.host + '/sockjs/websocket');
     webSocketProvider.setWebSocketURL(settings.webSocketURL);
 
-    $routeProvider
-      .when('/', {
+    $urlRouterProvider.otherwise('/main');
+
+    $stateProvider
+      .state('main', {
+        url: '/',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
   });
