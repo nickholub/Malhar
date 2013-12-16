00bc8642235850654a81db14c0548d1ee1ac73a4
Nick Holub <nikholub@gmail.com>
2013-12-16 10:52:05 -0800

    fix #515 Map Reduce Monitor - AngularJS Modules Definition

---
+++ web/mrmonitor/app/scripts/modules.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app.service', ['ng', 'restangular', 'ui.notify']);
+angular.module('app.filter', []);
+angular.module('app.directive', []);
+angular.module('app.controller', ['ngGrid', 'app.service']);
+
