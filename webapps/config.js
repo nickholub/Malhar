4ffbb86efe1b5145283d3ecb26c7eae2013ac0df
Nick Holub <nikholub@gmail.com>
2014-02-17 10:52:50 -0800

    fix #MLHR-788 Web Demos - Redis and MongoDB Config

--- webapps/config.js
+++ webapps/config.js
@@ -11,13 +11,12 @@ config.adsdimensions.redis = {};
 config.web.port = process.env.PORT || 3003;
 config.daemon.host = 'localhost';
 config.daemon.port = 9090;
-config.machine.redis.host = 'localhost';
+config.machine.redis.host = null;
 config.machine.redis.port = 6379;
 config.machine.redis.dbIndex = 2;
-//config.machine.forcedDelay = 5000; // for testing purpose only
-config.adsdimensions.redis.host = 'localhost';
+config.adsdimensions.redis.host = null;
 config.adsdimensions.redis.port = 6379;
-config.fraud.mongo.host = 'localhost';
+config.fraud.mongo.host = null;
 config.fraud.mongo.port = 27017;
 
 module.exports = config
