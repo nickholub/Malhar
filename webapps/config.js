93aba5e8ebc2635afa06e573e67ef7bda1681952
Nick Holub <nikholub@gmail.com>
2014-02-17 11:31:46 -0800

    fix #MLHR-790 Web Demos - Single Configuration File

--- webapps/config.js
+++ webapps/config.js
@@ -1,6 +1,6 @@
 var config = {};
 config.web = {};
-config.daemon = {};
+config.gateway = {};
 config.machine = {};
 config.machine.redis = {};
 config.fraud = {};
@@ -9,8 +9,8 @@ config.adsdimensions = {};
 config.adsdimensions.redis = {};
 
 config.web.port = process.env.PORT || 3003;
-config.daemon.host = 'localhost';
-config.daemon.port = 9090;
+config.gateway.host = 'localhost';
+config.gateway.port = 9090;
 config.machine.redis.host = null;
 config.machine.redis.port = 6379;
 config.machine.redis.dbIndex = 2;
@@ -19,5 +19,44 @@ config.adsdimensions.redis.port = 6379;
 config.fraud.mongo.host = null;
 config.fraud.mongo.port = 27017;
 config.fraud.mongo.dbName = 'frauddetect';
+
+// client settings (passed to the browser)
+config.settings = {};
+var settings = config.settings;
+
+settings.twitter = {};
+settings.mobile = {};
+settings.mobile.topic = {};
+settings.machine = {};
+settings.machine.range = {};
+settings.dimensions = {};
+settings.dimensions.range = {};
+settings.fraud = {};
+
+settings.webSocketUrl = 'ws://' + config.gateway.host + ':' + config.gateway.port + '/pubsub';
+settings.appsURL = 'http://' + config.gateway.host + ':' + config.gateway.port + '/static/#ops/apps/';
+
+settings.twitter.appName = 'TwitterCustomerApplication';
+settings.twitter.topic = 'demos.twitter.topURLs';
+settings.mobile.topic.out = 'demos.mobile.phoneLocationQueryResult';
+settings.mobile.topic.in = 'demos.mobile.phoneLocationQuery';
+settings.mobile.appName = 'MobileLocatorCustomerApplication';
+settings.machine.appName = 'MachineDataCustomerApplication';
+settings.machine.lookback = 180; // default lookback (minutes)
+settings.machine.metricformat = '#.0';
+settings.machine.pollInterval = 1000; // milliseconds
+settings.machine.range.customer = { start: 1, stop: 10 };
+settings.machine.range.product = { start: 4, stop: 6 };
+settings.machine.range.os = { start: 10, stop: 12 };
+settings.machine.range.software1 = { start: 10, stop: 12 };
+settings.machine.range.software2 = { start: 12, stop: 14 };
+settings.machine.range.deviceId = { start: 1, stop: 50 };
+settings.dimensions.appName = 'AdsDimensionCustomerApplication';
+settings.dimensions.lookback = 180; // default lookback (minutes)
+settings.dimensions.pollInterval = 1; // seconds
+settings.dimensions.range.publisher = { start: 0, stop: 49 };
+settings.dimensions.range.advertiser = { start: 0, stop: 99 };
+settings.dimensions.range.adunit = { start: 0, stop: 4 };
+settings.fraud.appName = 'FraudDetectionCustomerApplication';
 
 module.exports = config
