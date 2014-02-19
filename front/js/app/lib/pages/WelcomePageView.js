7cf1120a6dd5550e47ed858478e872947c62097e
Nick Holub <nikholub@gmail.com>
2014-02-18 19:17:35 -0800

    config widget

--- front/js/app/lib/pages/WelcomePageView.js
+++ front/js/app/lib/pages/WelcomePageView.js
@@ -22,6 +22,101 @@ var ConfigIssueCollection = DT.lib.ConfigIssueCollection;
 // widgets
 var ConfigWelcomeWidget = require('../widgets/ConfigWelcomeWidget');
 
+angular.module('app', ['ui.router'])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        templateUrl: 'views/test.html',
+        controller: 'MainCtrl'
+      })
+      .state('main.welcome', {
+        templateUrl: 'views/step_welcome.html'
+      })
+      .state('main.license', {
+        templateUrl: 'views/step_license.html'
+      })
+      .state('main.system', {
+        templateUrl: 'views/step_system.html'
+      })
+      .state('main.performance', {
+        templateUrl: 'views/step_performance.html'
+      })
+      .state('main.applications', {
+        templateUrl: 'views/step_applications.html'
+      })
+      .state('main.summary', {
+        templateUrl: 'views/step_summary.html'
+      });
+  })
+  .run(function ($state) {
+    $state.go('main.welcome');
+  })
+  .controller('MainCtrl', function ($scope, $state) {
+    $scope.value = new Date();
+
+    $scope.select = function (event, step) {
+      event.preventDefault();
+
+      console.log(step);
+      $scope.selStep = step;
+
+      $state.go('main.' + step.id);
+    };
+
+    $scope.steps = [
+      {
+        id: 'welcome',
+        label: 'Welcome!'
+      },
+      {
+        id: 'license',
+        label: 'License'
+      },
+      {
+        id: 'system',
+        label: 'System'
+      },
+      {
+        id: 'performance',
+        label: 'Performance'
+      },
+      {
+        id: 'applications',
+        label: 'Applications'
+      },
+      {
+        id: 'summary',
+        label: 'Summary'
+      }
+    ];
+
+    $scope.selStep = $scope.steps[0];
+  });
+
+ConfigWelcomeWidget = DT.lib.WidgetView.extend({
+
+  injections: 'app',
+
+  initialize1: function(options) {
+    console.log('__angular view');
+    console.log(options);
+  },
+
+  render: function () {
+    DT.lib.WidgetView.prototype.render.apply(this, _.toArray(arguments));
+
+    this.$('.widget-content').attr('ui-view', '');
+
+    angular.bootstrap(
+      this.$('.widget-content')[0],
+      ['app']
+    );
+
+    return this;
+  }
+
+});
+
 var WelcomePageView = BasePageView.extend({
 
     pageName: 'WelcomePageView',
